12eb3145ba99fe1e24853b864d3843a9363eda4e
Joe Burgess <joe@flatironschool.com>
2014-10-15 20:02:44 -0400

    finished

---
+++ ManifestDestiny/app/src/main/java/com/flatironschool/manifestdestiny/ManifestDestiny.java
@@ -0,0 +1,18 @@
+package com.flatironschool.manifestdestiny;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by joe on 10/15/14.
+ */
+public class ManifestDestiny extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_my);
+
+    }
+}
